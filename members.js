9c04d3f75834ab3866151b5cb207a9e9c373f5f5
Fernando Henrique <fernando.guerra@corujasdev.com.br>
2024-06-14 04:10:13 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  console.log("Skills: ");
+  console.log("1. JavaScript");
+  console.log("2. HTML");
+  console.log("3. CSS");
+  console.log("4. Node.js");
+  console.log("5. React.js");
+}
